0844cbbe0fc7aad9356c26df036e1a3097e26147
Awirut <awirut.ph@baac.or.th>
2018-07-09 14:55:28 +0700

    [Edited] เพิ่ม Query มาเป็น struct

--- main.go
+++ main.go
@@ -6,13 +6,53 @@ import (
     _ "github.com/go-sql-driver/mysql"
 )
 
+type UserData struct{
+    Id int 
+    CitizenId string
+    Firstname string
+    Lastname string
+    BirthYear int
+    FirstnameFather string
+    LastnameFather string
+    FirstnameMother string
+    LastnameMother string
+    SoldierId int
+    AddressId int
+}
+
 func main() {
-    _, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/testsck")
+    db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/testsck")
 
     if err != nil{
         fmt.Println("connect fail")
     }
 
     fmt.Println("connect success")
+
+    defer db.Close()
+
+    results,_ := db.Query("SELECT * FROM user")
+
+    for results.Next() {
+        var userData UserData
+        
+        err = results.Scan(
+            &userData.Id, 
+            &userData.CitizenId,
+            &userData.Firstname,
+            &userData.Lastname,
+            &userData.BirthYear,
+            &userData.FirstnameFather,
+            &userData.LastnameFather,
+            &userData.FirstnameMother,
+            &userData.LastnameMother,
+            &userData.SoldierId,
+            &userData.AddressId,
+        )
+        if err != nil {
+            panic(err.Error()) 
+        }
+        fmt.Println(userData)
+    }
 
 }
