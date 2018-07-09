f2d8a13a21466131dad9180ba089982dd5371e9c
Awirut <awirut.ph@baac.or.th>
2018-07-09 17:02:38 +0700

    [Edited] เพิ่มเรียกใช้ package users

--- main.go
+++ main.go
@@ -2,24 +2,10 @@ package main
 
 import (
     "fmt"
+    "sck/database_workshop/users"
     "database/sql"
     _ "github.com/go-sql-driver/mysql"
 )
-
-type UserData struct{
-    Id int 
-    CitizenId string
-    Firstname string
-    Lastname string
-    BirthYear int
-    FirstnameFather string
-    LastnameFather string
-    FirstnameMother string
-    LastnameMother string
-    SoldierId int
-    AddressId int
-}
-
 
 func main() {
     db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/testsck")
@@ -31,93 +17,10 @@ func main() {
     fmt.Println("connect success")
 
     defer db.Close()
-   // fmt.Println(read(db))
-    fmt.Println(readByCitizenId(db,"1552425252111"))
-   // fmt.Println(read(db))
+    users := users.Read(db)
+   fmt.Println(users)
+//     fmt.Println(readByCitizenId(db,"1552425252111"))
+//    fmt.Println(read(db))
 }
 
-
-func read(db *sql.DB) []UserData{
-    results,_ := db.Query("SELECT * FROM user")
-    var userDataList []UserData
-    for results.Next() {
-        var userData UserData
-        err := results.Scan(
-            &userData.Id, 
-            &userData.CitizenId,
-            &userData.Firstname,
-            &userData.Lastname,
-            &userData.BirthYear,
-            &userData.FirstnameFather,
-            &userData.LastnameFather,
-            &userData.FirstnameMother,
-            &userData.LastnameMother,
-            &userData.SoldierId,
-            &userData.AddressId,
-        )
-        if err != nil {
-            panic(err.Error()) 
-        }
-        userDataList = append(userDataList, userData)
-    }
-    return userDataList
-}
-
-func add(db *sql.DB) bool{
-    statement,_ := db.Prepare(`INSERT INTO user 
-    (citizen_id,firstname,lastname,birthyear
-    ,firstname_father,lastname_father,
-    firstname_mother,lastname_mother,soldier_id
-    ,address_id) 
-    VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ? ) `)
-    defer statement.Close()
-    _, err := statement.Exec("1552425252111","ชาติชาย2","เพ็ชรเม็ด","1985","สุชาติ","เพ็ชรเม็ด","สุณี","เพ็ชรเม็ด","8","1")
-		if err != nil {
-            panic(err.Error()) 
-            return false
-		}
-        return true
-}
-
-func remove(db *sql.DB,id string) bool{
-    statement,_ := db.Prepare("DELETE FROM  user WHERE user_id=?")
-    defer statement.Close()
-    _,err := statement.Exec(id)
-    if err != nil {
-        panic(err.Error()) 
-        return false
-    }
-    return true
-}
-
-func edit(db *sql.DB,id string,fatherName string) bool{
-    statement,_ := db.Prepare("UPDATE `user` SET firstname_father=? WHERE user_id=?")
-    defer statement.Close()
-    _,err := statement.Exec(fatherName,id)
-    if err != nil {
-        panic(err.Error()) 
-        return false
-    }
-    return true
-}
-
-func readByCitizenId(db *sql.DB,citizenId string) UserData{
-    var userData UserData
-    err := db.QueryRow("SELECT * FROM user WHERE citizen_id = ?",citizenId).Scan(
-        &userData.Id, 
-        &userData.CitizenId,
-        &userData.Firstname,
-        &userData.Lastname,
-        &userData.BirthYear,
-        &userData.FirstnameFather,
-        &userData.LastnameFather,
-        &userData.FirstnameMother,
-        &userData.LastnameMother,
-        &userData.SoldierId,
-        &userData.AddressId,
-    )
-    if err != nil {
-        panic(err)
-    }
-}
 
